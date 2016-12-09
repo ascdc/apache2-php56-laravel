ba905347f4b93c7a6b681bf14b94bd2e6530dbff
danny <asdc.sinica@gmail.com>
2016-12-09 09:21:21 +0800

    first commit

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 
 export PATH=$PATH:/root/.composer/vendor/bin
 
-find /var/www/html -type d -exec chmod 755 {} ;
-find /var/www/html -type d -exec chmod ug+s {} ;
-find /var/www/html -type f -exec chmod 644 {} ;
+find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html -type d -exec chmod ug+s {} \;
+find /var/www/html -type f -exec chmod 644 {} \;
 chown -R root:www-data /var/www/html
 chmod -R 775 /var/www/html/storage
 chmod -R 775 /var/www/html/bootstrap/cache/
